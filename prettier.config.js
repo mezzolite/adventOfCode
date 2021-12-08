d2c8f6c157ba347955eeaaf17bf15afa9b03667e
Mez Charney <mez@pana.com>
2021-12-08 10:40:40 -0700

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 100,
+  arrowParens: 'always',
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  semi: true,
+};
